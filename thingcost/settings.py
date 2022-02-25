a183fb43a2e1f836cbbf3999fbe1768d71235610
Sherwin Vargas <svargas025@gmail.com>
2022-02-25 15:59:08 -0600

    link

--- thingcost/settings.py
+++ thingcost/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'thingcost.netlify.app'
+    'thingcost.netlify.app',
 )
 
 # UPDATE THIS
